d0d0579b90c49e23efb26937694ad033366d9a91
Meghana <meghanabaggam@gmail.com>
2026-01-22 15:00:31 +0530

    little modifications

--- resume_builder/app/config/config.php
+++ resume_builder/app/config/config.php
@@ -1,5 +1,7 @@
 <?php
 
+use Phalcon\Config\Config;
+
 /*
  * Modified: prepend directory path of current file, because of this file own different ENV under between Apache and command line.
  * NOTE: please remove this comment.
@@ -7,7 +9,7 @@
 defined('BASE_PATH') || define('BASE_PATH', getenv('BASE_PATH') ?: realpath(dirname(__FILE__) . '/../..'));
 defined('APP_PATH') || define('APP_PATH', BASE_PATH . '/app');
 
-return new \Phalcon\Config\Config([
+return new Config([
     'database' => [
         'adapter'     => 'Mysql',
         'host'        => 'localhost',
@@ -16,6 +18,7 @@ return new \Phalcon\Config\Config([
         'dbname'      => 'resume_phalcon',
         'charset'     => 'utf8',
     ],
+    //projects stucture
     'application' => [
         'appDir'         => APP_PATH . '/',
         'controllersDir' => APP_PATH . '/controllers/',
--- resume_builder/public/index.php
+++ resume_builder/public/index.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 
 use Phalcon\Di\FactoryDefault;
+use Phalcon\Mvc\Application;
 
 error_reporting(E_ALL);
 
@@ -10,11 +11,11 @@ define('APP_PATH', BASE_PATH . '/app');
 
 try {
 
-    $di = new FactoryDefault();
+    $di = new FactoryDefault();//service container which automatically registers the all the services;
 
     // Load config
     $config = include APP_PATH . '/config/config.php';
-    $di->setShared('config', $config);
+    $di->setShared('config', $config);//register inside the DI container as a shared service
 
     // Load services (db, router, etc)
     include APP_PATH . '/config/services.php';
@@ -22,8 +23,9 @@ try {
     // Load autoloader
     include APP_PATH . '/config/loader.php';
 
-    $application = new \Phalcon\Mvc\Application($di);
-
+    $application = new Application($di);
+//It takes the incoming HTTP request, routes it to the correct controller,
+// executes your business logic, generates a response, and sends it back to the browser.
     echo $application->handle($_SERVER['REQUEST_URI'])->getContent();
 
 } catch (\Exception $e) {
